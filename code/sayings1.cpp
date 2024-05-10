9e5af3c45e7575cec970c84e1f995fc9c6a18bc7
Ming <yunyunruyue@gmail.com>
2024-05-10 13:44:54 +0800

    sayings1

---
+++ code/sayings1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "string1.h"
+const int ArSize = 10;
+const int MaxLen = 81;
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    String name;
+    cout << "Hi, what's your name?\n";
+    cin >> name;
+
+    cout << name << ", please enter"
+
+}
